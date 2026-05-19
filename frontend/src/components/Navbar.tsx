502251872ae69fcf643d7a1d9f1cc9f474954051
Iman <iman.tanovic@gmail.com>
2026-05-19 17:44:41 +0200

    css

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-return(<header className="home-navbar">
-        <div className="home-navbar__inner">
-          <Link to="/" className="home-navbar__brand">
-            BiznisProcjena.com
-          </Link>
+  return (
+    <header className="home-navbar">
+      <div className="home-navbar__inner">
+        <Link to="/" className="home-navbar__brand">
+          BiznisProcjena.com
+        </Link>
 
-          <nav className="home-navbar__nav">
-            <a href="#demo">Demo</a>
-            <Link to="/login" className="home-navbar__login">
-              Prijavi se
-            </Link>
-            <Link to="/register" className="home-navbar__login">
-              Registruj se
-            </Link>
-          </nav>
-        </div>
-      </header>)}
+        <nav className="home-navbar__nav">
+          <a href="#demo" className="home-navbar__demo">Demo</a>
+          <Link to="/login" className="home-navbar__cta">
+            Prijavi se
+          </Link>
+          <Link to="/register" className="home-navbar__text-link">
+            Registruj se
+          </Link>
+          
+        </nav>
+      </div>
+    </header>
+  );
+}
